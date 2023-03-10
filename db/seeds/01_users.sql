7e9a0a89209607d782491895ecf9134eca280262
Jason-Wall <jason.waldick@gmail.com>
2023-03-10 04:12:17 -0700

    Fix users and maps seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,25 +1,25 @@
 INSERT INTO users (
 name, email, password, user_url)
 VALUES (
-'Jenny Carroll', 'jenny@gmail.com', 'password');
+'Jenny Carroll', 'jenny@gmail.com', 'password', 'www.google.com');
 INSERT INTO users (
 name, email, password, user_url)
 VALUES (
-'Tyler Chessa', 'Tchessa@mail.com', 'password');
+'Tyler Chessa', 'Tchessa@mail.com', 'password', 'www.google.com');
 INSERT INTO users (
 name, email, password, user_url)
 VALUES (
-'Jason Waldick', 'WaldickJ@gmx.com', 'password');
+'Jason Waldick', 'WaldickJ@gmx.com', 'password', 'www.google.com');
 INSERT INTO users (
 name, email, password, user_url)
 VALUES (
-'Ariel Carroll', 'dogsarecute@mail.com', 'password');
+'Ariel Carroll', 'dogsarecute@mail.com', 'password', 'www.google.com');
 INSERT INTO users (
 name, email, password, user_url)
 VALUES (
-'Mike DiLuzio', 'ilovecars@outlook.com', 'password');
+'Mike DiLuzio', 'ilovecars@outlook.com', 'password', 'www.google.com');
 INSERT INTO users (
 name, email, password, user_url)
 VALUES (
-'Jessica Robinson', 'jr98@aol.com', 'password');
+'Jessica Robinson', 'jr98@aol.com', 'password', 'www.google.com');
 
--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,6 +1,6 @@
 INSERT INTO maps (
-user_id, title, description, map_url) VALUES (1, "Jenny's Map", "Dogs i've found!", "map url");
+user_id, map_title, map_description, map_url) VALUES (1, 'Jenny''s Map', 'Dogs i''ve found!', 'map url');
 INSERT INTO maps (
-user_id, title, description, map_url) VALUES (2, "Tyler's Map", "Restaurant", "map url");
+user_id, map_title, map_description, map_url) VALUES (2, 'Tyler''s Map', 'Restaurant', 'map url');
 INSERT INTO maps (
-user_id, title, description, map_url) VALUES (3, "Jason's Map", "Places to eat", "map url");
+user_id, map_title, map_description, map_url) VALUES (3, 'Jason''s Map', 'Places to eat', 'map url');
