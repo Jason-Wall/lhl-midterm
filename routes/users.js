ee420bc1c4958a14c524c73871a6ee32f845fa79
Jenny Carroll <jennygc@telus.net>
2023-03-11 18:18:12 +0000

    add login/id route

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,22 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+router.get("/", (req, res) => {
+  res.render("users");
+});
+
+// rather than having a user login, logout or registration, Andy suggested that we do the below:
+app.get("/login/id", (req, res) => {
+  // set an encrypted cookie
+  req.cookies.user_id = req.params.id;
+
+  //plaintext cookies
+  res.cookie("user_id", req.params.id);
+
+  //res.redirect(‘/’)
+});
 
 module.exports = router;
