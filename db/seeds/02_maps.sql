7d78a148ed7e687b874994e5ff22e29de844b33c
tylerchessa <tylerchessa@gmail.com>
2023-03-11 02:48:14 +0000

    fixed typo

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -3,4 +3,4 @@ user_id, title, description, map_url) VALUES (1, "Jenny's Map", "Dogs i've found
 INSERT INTO maps (
 user_id, title, description, map_url) VALUES (2, "Tyler's Map", "Restaurant", "map url");
 INSERT INTO maps (
-user_id, title, description, map_url) VALUES (3, "Jason's Map" "Places to eat", "map url");
+user_id, title, description, map_url) VALUES (3, "Jason's Map", "Places to eat", "map url");
