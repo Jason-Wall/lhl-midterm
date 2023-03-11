4dd7304c95509c61ec6da6053ff119f8fc9b285c
tylerchessa <tylerchessa@gmail.com>
2023-03-11 04:39:15 +0000

    create map 4 with pins, Fast Food

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -4,3 +4,10 @@ INSERT INTO maps (
 user_id, map_title, map_description, map_url) VALUES (2, 'Tyler''s Map', 'Restaurant', 'map url');
 INSERT INTO maps (
 user_id, map_title, map_description, map_url) VALUES (3, 'Jason''s Map', 'Places to eat', 'map url');
+
+
+
+INSERT INTO maps (user_id, map_title, map_description, map_url)
+VALUES (2, 'Fast Food', 'Top Fast Food Restaurants in downtown Vancouver', 'map url');
+
+
--- db/seeds/05_pins.sql
+++ db/seeds/05_pins.sql
@@ -12,3 +12,19 @@ VALUES (2, 1, 'title', 'description', 'pin_url', 109, 093);
 
 INSERT INTO pins (map_id, user_id, pin_title, pin_description, pin_url, latitude, longitude)
 VALUES (3, 2, 'title', 'description', 'pin_url', 109, 093);
+
+
+-- pins for map 1 (top fast food resteraunts)
+INSERT INTO pins (map_id, user_id, pin_title, pin_description, pin_url, latitude, longitude)
+VALUES (4, 2, 'Five Guys', 'Always fresh, never frozen', 'pin_url', 49.281059, -123.119019);
+
+INSERT INTO pins (map_id, user_id, pin_title, pin_description, pin_url, latitude, longitude)
+VALUES (4, 2, 'Fatburger', 'The Last Great Hamburger Stand', 'pin_url', 49.281059, -123.119019);
+
+INSERT INTO pins (map_id, user_id, pin_title, pin_description, pin_url, latitude, longitude)
+VALUES (4, 2, 'Chicken World', 'When Dad''s Not Cooking', 'pin_url', 49.281059, -123.119019);
+
+
+
+
+
