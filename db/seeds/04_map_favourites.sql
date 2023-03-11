8825c00d8284924520ef6814c81caaa9241cb1b6
tylerchessa <tylerchessa@gmail.com>
2023-03-11 02:45:35 +0000

    create seeds

---
+++ db/seeds/04_map_favourites.sql
@@ -0,0 +1,14 @@
+INSERT INTO map_favourites (user_id, map_id)
+VALUES (1, 1)
+
+INSERT INTO map_favourites (user_id, map_id)
+VALUES (2, 2)
+
+INSERT INTO map_favourites (user_id, map_id)
+VALUES (3, 3)
+
+INSERT INTO map_favourites (user_id, map_id)
+VALUES (4, 4)
+
+INSERT INTO map_favourites (user_id, map_id)
+VALUES (5, 5)
