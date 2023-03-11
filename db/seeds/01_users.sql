52050a3f69fac3a38e7f7649aa248ffefd8feef0
Jenny Carroll <jennygc@telus.net>
2023-03-11 02:58:23 +0000

    create seeds for users and maps

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,25 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (
+name, email, password, user_url)
+VALUES (
+'Jenny Carroll', 'jenny@gmail.com', 'password');
+INSERT INTO users (
+name, email, password, user_url)
+VALUES (
+'Tyler Chessa', 'Tchessa@mail.com', 'password');
+INSERT INTO users (
+name, email, password, user_url)
+VALUES (
+'Jason Waldick', 'WaldickJ@gmx.com', 'password');
+INSERT INTO users (
+name, email, password, user_url)
+VALUES (
+'Ariel Carroll', 'dogsarecute@mail.com', 'password');
+INSERT INTO users (
+name, email, password, user_url)
+VALUES (
+'Mike DiLuzio', 'ilovecars@outlook.com', 'password');
+INSERT INTO users (
+name, email, password, user_url)
+VALUES (
+'Jessica Robinson', 'jr98@aol.com', 'password');
+
--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -1,4 +1,6 @@
--- Widgets table seeds here (Example)
-INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
-INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
-INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+INSERT INTO maps (
+user_id, title, description, map_url) VALUES (1, "Jenny's Map", "Dogs i've found!", "map url");
+INSERT INTO maps (
+user_id, title, description, map_url) VALUES (2, "Tyler's Map", "Restaurant", "map url");
+INSERT INTO maps (
+user_id, title, description, map_url) VALUES (3, "Jason's Map" "Places to eat", "map url");
