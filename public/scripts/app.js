f3a69cc88678c240bd8c40cd5b281337d1c694f3
Jenny Carroll <jennygc@telus.net>
2023-03-14 22:40:58 +0000

    resolve conflicts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -44,18 +44,18 @@ $(document).ready(() => {
 //   });
 // });
 
-const populateMapsList = () => {
-  $(".logo").on("click", () => {
-    $.ajax({
-      type: "GET",
-      url: "/maps",
-    })
-      .then((maps) => renderMapsList(maps, 'mapListContainer'))
-      .catch(function (xhr, status, error) {
-        console.log("Error: " + error);
-      });
-  });
-};
+// const populateMapsList = () => {
+//   $(".logo").on("click", () => {
+//     $.ajax({
+//       type: "GET",
+//       url: "/maps",
+//     })
+//       .then((maps) => renderMapsList(maps, "mapListContainer"))
+//       .catch(function (xhr, status, error) {
+//         console.log("Error: " + error);
+//       });
+//   });
+// };
 
 const populateMapArea = (mapID) => {
   console.log(mapID);
@@ -66,7 +66,7 @@ const populateMapArea = (mapID) => {
       //data: {mapid: mapID} //This will be received through req.body in the backend route
       // data: {mapid: mapID}
     })
-        .then (({map, api}) => {
+      .then(({ map, api }) => {
         renderMapArea(map, api);
       })
       .catch(function (xhr, status, error) {
@@ -86,30 +86,31 @@ const populateMapArea = (mapID) => {
       renderMemberArea(response.user);
     });
   });
-  //the below handler was not working for .logout button because it was created after the document loaded.  I got around this by targeting the parent .buttons and then adding a second parameter to the .on function of .logout
+
+  //logout event listener
   $(".buttons").on("click", ".logout", () => {
     $.ajax({
       type: "POST",
-      //change the below to /users/logout
       url: "/users-api/logout",
     }).then((response) => {
       console.log(response);
       resetNavArea();
     });
   });
-  //when a user clicks Discover Maps button, it loads the maps
+
+  //discover maps listener
   $(".buttons").on("click", ".discover", () => {
     $(".mainContainer").empty();
     const $discoverMaps = `<div class="discoverMapsArea">
-    <div class="discoverMapsTitle">Discover Maps!</div>
-    <section class="mapListContainer">
-    </section>
-  </div>
-  <div class="mapArea">No potatoes here</div>`;
+      <div class="discoverMapsTitle">Discover Maps!</div>
+      <section class="mapListContainer">
+      </section>
+    </div>
+    <div class="mapArea">No potatoes here</div>`;
     $(".mainContainer").append($discoverMaps);
   });
 
-  //when a user clicks My Maps button, it loads their maps
+  //my maps listener
   $(".buttons").on("click", ".myMaps", () => {
     $.ajax({
       type: "POST",
@@ -120,4 +121,83 @@ const populateMapArea = (mapID) => {
       renderMemberArea(response.user);
     });
   });
+
+  //create a map listener (takes you to createMapModal)
+  // $(".buttons").on("click", ".createAMap", () => {
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "/users-api/myinfo",
+  //   }).then((data) => {
+  //     renderMapModal(createMapForm, data.user[0].id);
+  //   });
+  // });
 };
+
+const populateMapsList = () => {
+  $(".logo").on("click", () => {
+    $.ajax({
+      type: "GET",
+      url: "/maps",
+    })
+      .then((maps) => renderMapsList(maps))
+      .catch(function (xhr, status, error) {
+        console.log("Error: " + error);
+      });
+  });
+};
+
+// const populateMapArea = (mapID) => {
+//   console.log(mapID);
+//   $(`#${mapID}`).on("click", () => {
+//     $.ajax({
+//       type: "GET",
+//       url: `/maps/${mapID}`, //this will be received in the backend route with the help of req.params
+//       //data: {mapid: mapID} //This will be received through req.body in the backend route
+//       // data: {mapid: mapID}
+//     })
+//       .then((map) => {
+//         renderMapArea(map);
+//       })
+//       .catch(function (xhr, status, error) {
+//         console.log("Error: " + error);
+//       });
+//   });
+// };
+
+//Jasons demo:
+// const addNewDiv = () => {
+//   $('.discoverMapsTitle').on('click', () => {
+//     $.ajax({
+//       type: 'GET',
+//       url: '/maps',
+//     })
+//       .then((names) => {
+//         console.log(names);
+//         for (let entry of names) {
+//           const newDiv = $('<div>').text(entry.name);
+//           $('.discoverMapsArea').append(newDiv);
+//         };
+//       })
+//       .catch(function (xhr, status, error) {
+//         console.log("Error: " + error);
+//       })
+//   })
+// }
+
+//users example
+// Client facing scripts here
+// $(() => {
+//   $(".login").on("click", () => {
+//     $.ajax({
+//       method: "GET",
+//       url: "/api/users",
+//     }).done((response) => {
+//       const $usersList = $("#users");
+//       $usersList.empty();
+
+//       for (const user of response.users) {
+//         $(`<li class="user">`).text(user.name).appendTo($usersList);
+//       }
+//     });
+//   });
+// });
