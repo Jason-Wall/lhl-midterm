f716e1e5cfb1c6714bc3d4b22bedaf3adf56a9da
Jenny Carroll <jennygc@telus.net>
2023-03-15 16:42:57 +0000

    fix bug in select statements re fav pins and fav maps;

--- db/queries/user.js
+++ db/queries/user.js
@@ -3,7 +3,10 @@ const db = require("../connection");
 //4this query returns the data to users-api .then of the route /myinfo
 const getUserData = (userId) => {
   return db
-    .query(`SELECT maps.*, users.name FROM maps JOIN users ON user_id = users.id WHERE maps.user_id = $1;`, [userId])
+    .query(
+      `SELECT maps.*, users.name FROM maps JOIN users ON user_id = users.id WHERE maps.user_id = $1;`,
+      [userId]
+    )
     .then((maps) => {
       return db
         .query(`SELECT * FROM pins WHERE pins.user_id = $1;`, [userId])
@@ -25,13 +28,13 @@ const getUserData = (userId) => {
                     .then((user) => {
                       return db
                         .query(
-                          `SELECT maps.*, users.name FROM maps JOIN users ON user_id = users.id JOIN map_favourites ON maps.id = map_id WHERE maps.user_id = $1`,
+                          `SELECT maps.*, users.name FROM maps JOIN users ON user_id = users.id JOIN map_favourites ON maps.id = map_id WHERE map_favourites.user_id = $1`,
                           [userId]
                         )
                         .then((favMaps) => {
                           return db
                             .query(
-                              `SELECT * FROM pins JOIN pin_favourites ON pins.id = pin_id WHERE pins.user_id = $1`,
+                              `SELECT * FROM pins JOIN pin_favourites ON pins.id = pin_id WHERE pin_favourites.user_id = $1`,
                               [userId]
                             )
                             .then((favPins) => {
