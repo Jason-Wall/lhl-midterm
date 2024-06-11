7dd3c009d27fe9c5513777d7c1bf253299c6f469
Jenny Carroll <jennygc@telus.net>
2024-06-11 09:27:34 -0700

    update ssl in connection

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 // PG database client/connection setup
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const dbParams = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: true,
 };
 
 const db = new Pool(dbParams);
